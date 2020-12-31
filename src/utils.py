4939cf67ad27eb963b29005b11c447168f2194b8
Alejandro Santorum <alejandro.santorum@gmail.com>
2020-12-31 11:48:54 +0100

    Add split_train_test utils

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,35 @@ import numpy as np
 from collections import Counter
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, plot_confusion_matrix
 
+from preprocessing import split_series_byID
+
+
+
+def split_train_test(dataframe, train_perc):
+    '''
+        INPUT:
+            dataframe: pandas dataframe
+            train_perc: float between 0 and 1. Train set percentage.
+        
+        OUTPUT:
+            xtrain: pandas dataframe. X train set
+            ytrain: pandas dataframe. Y train set
+            xtest: pandas dataframe. X test set
+            ytest: pandas dataframe. Y test set
+
+        DESCRIPTION:
+            It separates given pandas dataframe in 4 sub-dataframes:
+            X train set, Y train set, X test set, Y test set.
+    '''
+    N_TOTAL_IDS = 100
+    df_train, df_test = split_series_byID(N_TOTAL_IDS, train_perc, dataframe)
+    features = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'Temp.', 'Humidity']
+    xtrain, ytrain = df_train[features].values, df_train['class'].values
+    xtest, ytest = df_test[features].values, df_test['class'].values
+    return xtrain, ytrain, xtest, ytest
+
+
+
 
 def metric_report(y_true, y_pred, plot_conf_mtx=False, clf=None, X=None):
     '''
@@ -31,25 +60,28 @@ def metric_report(y_true, y_pred, plot_conf_mtx=False, clf=None, X=None):
 
     conf_m = confusion_matrix(y_true, y_pred, labels=['background', 'banana', 'wine'])
 
-    recall_bg = conf_m[0][0]/sum(conf_m[:,0])   # predicted_bg / total_real_bg
-    recall_ban = conf_m[1][1]/sum(conf_m[:,1])   # predicted_banana / total_real_banana
-    recall_wine = conf_m[2][2]/sum(conf_m[:,2]) # predicted_wine / total_real_wine
+    recall_bg = conf_m[0][0]/sum(conf_m[0,:])   # predicted_bg / total_real_bg
+    recall_ban = conf_m[1][1]/sum(conf_m[1,:])   # predicted_banana / total_real_banana
+    recall_wine = conf_m[2][2]/sum(conf_m[2,:]) # predicted_wine / total_real_wine
 
-    print('-------------------------------------------')
-    print('Real background percentage:', bg_perc)
-    print('Real banana percentage:', bg_banana)
-    print('Real wine percentage:', bg_wine)
+    print('==============================================')
+    print('TEST SET PROPORTIONS:')
+    print('\tReal background percentage:', bg_perc)
+    print('\tReal banana percentage:', bg_banana)
+    print('\tReal wine percentage:', bg_wine)
+    print('------------------------------------------')
     print('Accuracy:', accuracy_score(y_true, y_pred))
     print('Recall on background:', recall_bg)
     print('Recall on banana:', recall_ban)
     print('Recall on wine:', recall_wine)
     print('F1-score:', f1_score(y_true, y_pred, average='weighted'))
     if plot_conf_mtx:
-        if not (clf and X):
+        if clf is None or X is None:
             raise ValueError('To plot confusion matrix is required estimator and input X')
         else:
             plot_confusion_matrix(clf, X, y_true)
-    print('-------------------------------------------')
+    print('==============================================')
+
 
 
 
